a58702eafce3440163e53a60965ba1e055886c6e
James Randolph <james.randolph2016@gmail.com>
2025-02-03 14:04:22 -0500

    add to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,31 @@
-import { Button } from '@/components/ui/button'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
+  const documents = [];
   return (
-    <div>
-      <Button />
+    <main className='home-container'>
+      <Header className='sticky left-0 top-0'>
+
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+{documents.length > 0 ? (
+  <div>
+
+  </div>
+): (
+  <div>
     </div>
+)}
+  
+    </main>
   )
 }
 
