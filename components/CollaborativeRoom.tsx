cee9e836b65748f0df14b592c1d5a00e9c19182b
James Randolph <james.randolph2016@gmail.com>
2025-02-04 09:25:12 -0500

    add state to change document title header

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,19 +1,30 @@
 'use client'
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import { Editor } from './editor/Editor'
 import ActiveCollaborators from './ActiveCollaborators'
+import { Input } from "@/components/ui/input"
+
 
 const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false)
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+
   return (
     <RoomProvider id={roomId}>
     <ClientSideSuspense fallback={<div>Loading…</div>}>
       <div className='collaborative-room'>
       <Header>
-            <div className="flex w-fit items-center justify-center gap-2">
-                <p className='document-title'>Document title goes here</p>
+            <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
+                {editing && !loading ? (
+                  <Input />
+                )}
             </div> 
             <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
                 <ActiveCollaborators />
