0ec843b2d8466da227e016d0f8d68b4b17438ca9
James Randolph <james.randolph2016@gmail.com>
2025-02-06 10:07:19 -0500

    fix typo in type declaration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,7 @@ import { getDocuments } from '@/lib/actions/room.actions';
 import { dateConverter } from '@/lib/utils';
 import { SignedIn, UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server';
+import { RoomData } from '@liveblocks/node';
 import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
@@ -19,9 +20,39 @@ interface RoomDocument {
 
 const Home = async () => {
   const clerkUser = await currentUser();
-  if(!clerkUser) redirect('/sign-in');
+  if (!clerkUser) redirect('/sign-in');
 
   const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
+
+
+  if (!roomDocuments || !roomDocuments.data || roomDocuments.data.length === 0) {
+    return (
+      <main className="home-container">
+        <Header className="sticky left-0 top-0">
+          <div className="flex items-center gap-2 lg:gap-4">
+            <Notifications />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </Header>
+
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="home-container">
@@ -34,55 +65,38 @@ const Home = async () => {
         </div>
       </Header>
 
-      {roomDocuments.data.length > 0 ? (
-        <div className="document-list-container">
-          <div className="document-list-title">
-            <h3 className="text-28-semibold">All documents</h3>
-            <AddDocumentBtn 
-              userId={clerkUser.id}
-              email={clerkUser.emailAddresses[0].emailAddress}
-            />
-          </div>
-          <ul className="document-ul">
-            {roomDocuments.data.map(({ id, metadata, createdAt }: RoomDocument)
-              <li key={id} className="document-list-item">
-                <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
-                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
-                    <Image 
-                      src="/assets/icons/doc.svg"
-                      alt="file"
-                      width={40}
-                      height={40}
-                    />
-                  </div>
-                  <div className="space-y-1">
-                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
-                    <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
-                  </div>
-                </Link>
-                <DeleteModal roomId={id} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ): (
-        <div className="document-list-empty">
-          <Image 
-            src="/assets/icons/doc.svg"
-            alt="Document"
-            width={40}
-            height={40}
-            className="mx-auto"
-          />
-
-          <AddDocumentBtn 
+      <div className="document-list-container">
+        <div className="document-list-title">
+          <h3 className="text-28-semibold">All documents</h3>
+          <AddDocumentBtn
             userId={clerkUser.id}
             email={clerkUser.emailAddresses[0].emailAddress}
           />
         </div>
-      )}
+        <ul className="document-ul">
+          {roomDocuments.data.map(({ id, metadata, createdAt }: RoomData) => (
+            <li key={id} className="document-list-item">
+              <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                  <Image
+                    src="/assets/icons/doc.svg"
+                    alt="file"
+                    width={40}
+                    height={40}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                  <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
+                </div>
+              </Link>
+              <DeleteModal roomId={id} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
