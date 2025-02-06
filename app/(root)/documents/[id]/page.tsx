e75127eb6bff89a5bc9c527efc2e23d7c1d3339c
James Randolph <james.randolph2016@gmail.com>
2025-02-06 10:23:09 -0500

    add types

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,26 @@
+type RoomAccess = 'room:write' | 'room:read';
+
+interface Room {
+ usersAccesses: {
+   [email: string]: RoomAccess[]
+ };
+ metadata: any;
+}
+
+interface User {
+ email: string;
+ id: string;
+ firstName?: string;
+ lastName?: string;
+ imageUrl?: string;
+}
+
+interface SearchParamProps {
+ params: {
+   id: string;
+ };
+}
+
 import CollaborativeRoom from "@/components/CollaborativeRoom"
 import { getDocument } from "@/lib/actions/room.actions";
 import { getClerkUsers } from "@/lib/actions/user.actions";
@@ -5,41 +28,41 @@ import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation";
 
 const Document = async ({ params }: SearchParamProps) => {
-  const { id } = params;
-  const clerkUser = await currentUser();
-  if(!clerkUser) redirect('/sign-in');
+ const { id } = params;
+ const clerkUser = await currentUser();
+ if(!clerkUser) redirect('/sign-in');
 
-  const room = await getDocument({
-    roomId: id,
-    userId: clerkUser.emailAddresses[0].emailAddress,
-  });
+ const room = await getDocument({
+   roomId: id,
+   userId: clerkUser.emailAddresses[0].emailAddress,
+ });
 
-  if(!room) redirect('/');
+ if(!room) redirect('/');
 
-  const userIds = Object.keys(room.usersAccesses);
-  const users = await getClerkUsers({ userIds });
+ const userIds = Object.keys(room.usersAccesses);
+ const users = await getClerkUsers({ userIds });
 
-  const usersData = users.filter((user): user is User => user !== null).map((user) => ({
-    ...user,
-    userType: room.usersAccesses[user.email]?.includes('room:write')
-      ? 'editor'
-      : 'viewer'
-  }));
-  
-  
+ const usersData = users.filter((user): user is User => user !== null).map((user) => ({
+   ...user,
+   userType: room.usersAccesses[user.email]?.includes('room:write')
+     ? 'editor'
+     : 'viewer'
+ }));
 
-  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
+ const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') 
+   ? 'editor' 
+   : 'viewer';
 
-  return (
-    <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom 
-        roomId={id}
-        roomMetadata={room.metadata}
-        users={usersData}
-        currentUserType={currentUserType}
-      />
-    </main>
-  )
+ return (
+   <main className="flex w-full flex-col items-center">
+     <CollaborativeRoom 
+       roomId={id}
+       roomMetadata={room.metadata}
+       users={usersData}
+       currentUserType={currentUserType}
+     />
+   </main>
+ )
 }
 
 export default Document
